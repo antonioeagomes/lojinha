a3ebee3f932dc98d519d6c25288dc6cdd59e4d58
Antonio Emmanuel de Araujo Gomes <emmanuelgomes@pmenos.com.br>
2021-04-12 09:37:43 -0300

    Service Domain

---
+++ src/Store.Catalogo.Domain/EstoqueService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Catalogo.Domain
+{
+    /*
+     * Cross aggregate
+     * Quando a regra de negócio não cabe na aolicação nem na entidade
+     */
+    public class EstoqueService : IEstoqueService
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public EstoqueService(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
+        {
+            var produto = await _produtoRepository.ObterPorId(produtoId);
+
+            if (produto == null) return false;
+
+            if (!produto.PossuiEstoque(quantidade)) return false;
+
+            produto.DebitarEstoque(quantidade);
+
+            _produtoRepository.Atualizar(produto);
+            return await _produtoRepository.UnitOfwork.Commit();
+        }      
+
+        public async Task<bool> ReporEstoque(Guid produtoId, int quantidade)
+        {
+            var produto = await _produtoRepository.ObterPorId(produtoId);
+
+            if (produto == null) return false;            
+
+            produto.ReporEstoque(quantidade);
+
+            _produtoRepository.Atualizar(produto);
+            return await _produtoRepository.UnitOfwork.Commit();
+        }
+        public void Dispose()
+        {
+            _produtoRepository.Dispose();
+        }
+    }
+}
